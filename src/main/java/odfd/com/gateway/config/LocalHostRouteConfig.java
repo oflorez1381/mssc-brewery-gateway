73c8566937c3cc0cb2f60e64386cf04976118c25
oflorez1381 <oscarflorez1381@gmail.com>
2020-12-30 16:38:30 -0500

    feat(19-256): Beer Order Service Route Configuration

--- src/main/java/odfd/com/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/odfd/com/gateway/config/LocalHostRouteConfig.java
@@ -14,6 +14,8 @@ public class LocalHostRouteConfig {
                 .route("beer-service",
                         r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                                 .uri("http://localhost:8081"))
+                .route("order-service", r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8082"))
                 .build();
     }
 
