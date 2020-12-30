aaa3111ca91bd3922be5a2f0ec0ecdccc3cd5b8f
oflorez1381 <oscarflorez1381@gmail.com>
2020-12-30 15:16:39 -0500

    feat(19-254): adding example route

---
+++ src/main/java/odfd/com/gateway/config/GoogleConfig.java
@@ -0,0 +1,22 @@
+package odfd.com.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleConfiguration(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/googlesearch")
+                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)","/${segment}"))
+                    .uri("https://google.com"))
+                .build();
+    }
+
+}
